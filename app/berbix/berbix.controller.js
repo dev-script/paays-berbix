8416375ec60397b5f78be985cd588cebae7bc8ba
dev-script <shekhawat.dev313@gmail.com>
2022-06-18 13:58:54 +0530

    IP address

--- app/berbix/berbix.controller.js
+++ app/berbix/berbix.controller.js
@@ -37,7 +37,7 @@ module.exports = function (app) {
                 const isValidEmail = re.test(dealerEmail.toLowerCase());
                 if (!isValidEmail) throw new Error('Invalid email');
                 if (!isValid) throw new Error('Invalid phone number');
-                const requestedIP = req.ip;
+                const requestedIP = '136.144.17.67' //req.ip;
                 const validIp = constants.REGEX_IP_ADDRESS.test(requestedIP);
                 if (!validIp) {
                     throw new Error('invalid user ip address');
